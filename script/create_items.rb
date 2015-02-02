eb9c0d8cae610ec70eb2024217a01234bb2524f0
Louie Arencibia <louie728@gmail.com>
2015-02-02 15:57:18 -0500

    - Item import

--- script/create_items.rb
+++ script/create_items.rb
@@ -17,6 +17,9 @@ products_created = 0
 variants_created = 0
 error_items = 0
 
+@local_site_path = "/home/louie/Dropbox/DEV/Artistic/sitesucker/www.artisticribbon.com/"
+
+
 # Properties
 @properties_hash = {}
 # color
@@ -36,7 +39,7 @@ if @type_prop.nil?
       presentation: "Type"
   )
 end
-@properties_hash.merge!(type: [@type_prop,"item_type"] )
+@properties_hash.merge!(type: [@type_prop,"@item_type"] )
 
 
 # Brand
@@ -74,7 +77,7 @@ end
 
 
 #Bow Options
-@bow_option_hash
+@bow_option_hash = {}
 
 bow_size_option = Spree::OptionType.find_by_name('bow-size')
 if bow_size_option.nil?
@@ -89,6 +92,9 @@ end
 sorted_web_items =
 
 previous_page = " "
+
+@position = 1
+
 WebItem.all.order(:page).all.each do |wi|
 
     cur_page = wi.page
@@ -103,6 +109,7 @@ WebItem.all.order(:page).all.each do |wi|
       next
     end
 
+    is_master = false
 
     if !(previous_page.strip == cur_page.strip)
       # if we are here we have to create a product.
@@ -113,16 +120,19 @@ WebItem.all.order(:page).all.each do |wi|
         stand_alone_product = true
       end
 
-      item_type = item_type(rcpbs)
+      is_master = true
+      @position = 1
+
+      @item_type = item_type(rcpbs) ||  ''
 
       # find taxon record
-      srchtype = (item_type.blank?) ? item_type.strip:%Q{#{item_type.downcase.strip}s}
+      srchtype = (@item_type.blank?) ? @item_type.strip : %Q{#{@item_type.downcase.strip}s}
       maincat = get_formed_cat_name(wi.cat)
       subcat = wi.subcat.downcase.strip.titlecase
 
       perma_srch = %Q{'%#{srchtype}%#{maincat}%#{subcat}%'}
 
-      taxonrec = Spree::Taxon.where("permalink like #{perma_srch} ")
+      taxonrec = Spree::Taxon.where("permalink like #{perma_srch} ").first
       #
       @product = Spree::Product.find_by_name(wi.title)
       if !@product.nil?
@@ -131,27 +141,35 @@ WebItem.all.order(:page).all.each do |wi|
       else
         @product = Spree::Product.new(
           name: wi.title.strip.titlecase,
-          description:wi,top_description,
-          available_on: Date.today(),
+          description: wi.top_description,
+          available_on: Date.today()-1.day,
           shipping_category_id: 1 ,
           meta_keywords: wi.keywords,
-          price: rcpbs.price.to_f
+          price: rcpbs.rc_price.to_f,
+          sku: rcpbs.new_pbs_desc_1
         )
 
 
+        @product.save!
         #create taxon for product
-        if !taxonrec.nil?
-          @product << taxonrec
-          @product.save
+        existing_taxon_array = @product.taxons.map{|t| t.id}
+        if !taxonrec.nil? && !existing_taxon_array.include?(taxonrec.id)
+          @product.taxons << taxonrec
+          @product.save!
         end
+
+
 
         array_of_properties = @product.properties.map{|p| p.name}
 
         @properties_hash.each do |k,v|
+
+
           if !array_of_properties.include?(v.first.name)
             @product.properties << v.first
             @product.save!
           end
+
           propval = Spree::ProductProperty.find_by_product_id_and_property_id(
             @product.id,v.first.id
           )
@@ -162,25 +180,115 @@ WebItem.all.order(:page).all.each do |wi|
                 value: eval(v.last)
             )
           else
-            propval.update_attribute(value: eval(v.last) )
+            propval.update_attribute(:value, eval(v.last) )
           end
         end
 
+        array_of_options = @product.option_types.map{|ot|ot.name}
+
+        option_hash = {}
+
+        if @item_type == "Ribbon"
+          option_hash = @ribbon_option_hash
+        elsif @item_type == "Bow"
+          option_hash = @bow_option_hash
+        end
+
+        option_hash.each do |k,v|
+
+          if !array_of_options.include?(v.name)
+            @product.option_types << v
+            @product.save!
+          end
+
+          opval = Spree::ProductOptionType.find_by_product_id_and_option_type_id(
+              @product.id,v.id
+          )
+          if opval.nil?
+            opval = Spree::ProductOptionType.create(
+                product_id:@product.id,
+                option_type_id: v.id
+            )
+          end
+        end
+
+        #create product image
+        image_path = %Q{#{@local_site_path}#{wi.image_file[/images.*/i,0]}}
+        if File.exists?(image_path)
+          @product.images <<  Spree::Image.create!(:attachment => File.open(image_path))
+          @product.save!
+        end
 
         logger.info "Product #{wi.title} created in Spree"
         puts "Product #{wi.title} created in Spree"
+      end
+    else
+      @position += 1
+      # create Variant
+      v = Spree::Variant.new(
+          sku: rcpbs.new_pbs_desc_1,
+          product_id: @product.id,
+          is_master: is_master,
+          price: rcpbs.rc_price,
+          cost_currency: "USD",
+          track_inventory: true,
+          tax_category_id: 1,
+          stock_items_count: 1
+      )
+      v.save!
 
-        # create Master Variant
+      # set options
+      ot = v.product.option_types
+      if !ot.empty?
+        array_of_type_and_id = ot.map{|o|[o.id,o.name]}
+
+        array_of_type_and_id.each do |av|
+          val = ''
+          srcval = ''
+          if av.last == "ribbon-width"
+            val = rcpbs.width
+            srcval = val
+          elsif av.last == "ribbon-putup"
+            val = rcpbs.putup_pack
+            srcval = val.strip.gsub(' ','-')
+          else
+            val = rcpbs.new_pbs_desc_3.split(",").last.strip.titlecase rescue ''
+            srcval = val
+          end
+
+          if !val.empty?
+            sov = Spree::OptionValue.find_by_option_type_id_and_name(
+                av.first,srcval
+            )
+            if sov.nil?
+              sov = Spree::OptionValue.create(
+                  option_type_id: av.first,
+                  presentation: val,
+                  name: srcval
+              )
+            end
+            v.option_values << sov
+            v.save!
+          end
+        end
 
       end
+      #create product image
+      image_path = %Q{#{@local_site_path}#{wi.image_file[/images.*/i,0]}}
+      if File.exists?(image_path)
+        v.images <<  Spree::Image.create!(:attachment => File.open(image_path))
+        v.save!
+      end
+
     end
 
 
 
+    logger.info "Variant #{rcpbs.new_pbs_desc_1} created in Spree"
+    puts "Variant #{rcpbs.new_pbs_desc_1} created in Spree"
 
+    previous_page = cur_page.strip
 end
-
-
 
 
 
@@ -205,6 +313,7 @@ BEGIN{
     t = sku.split('-')
     %Q{#{t[0]}-#{t[1]}}
   end
+
 
 
 }
