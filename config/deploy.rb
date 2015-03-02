cdb66d5f00bad363e68399a526a595474b0d5444
Dan Sadaka <dan@web-site1.com>
2015-03-02 14:08:51 -0500

    Still Switching from rubber to capistrano and from mysql to SQL in project spree

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,23 @@
 # config valid only for current version of Capistrano
 lock '3.3.5'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'artspree3'
+set :repo_url, 'git@github.com:web-site1/spree.git'
 
 # Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, "/var/www/#{fetch(:application)}"
 
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
+set :ssh_options, { :forward_agent => true }
+set :deploy_via, :remote_cache
+
+# Setup rvm
+set :rvm_type, :user                     # Defaults to: :auto
+set :rvm_ruby_version, '2.1.5@artspree3 --create'      # Defaults to: 'default'
 
 # Default value for :format is :pretty
 # set :format, :pretty
@@ -23,10 +29,10 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
