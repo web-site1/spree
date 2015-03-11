108958ddea64778e338ebf6efdb86aaa488555b0
Louie Arencibia <louie728@gmail.com>
2015-03-11 15:03:25 -0400

    - Fixes for upload

--- script/create_items.rb
+++ script/create_items.rb
@@ -329,19 +329,32 @@ CSV.open(csv_error_file, "wb") do |csv|
 
             found_image = false
             if @wi
-              #create swatch image if it exsists
-              swatch_image_path = %Q{#{@local_site_path}#{@wi.swatch_image_file[/images.*/i,0]}} rescue ''
-              if File.exists?(swatch_image_path)
-                @product.images <<  Spree::Image.create!(:attachment => File.open(swatch_image_path))
-                @product.save!
+
+              begin
+                #create swatch image if it exsists
+                swatch_image_path = %Q{#{@local_site_path}#{@wi.swatch_image_file[/images.*/i,0]}} rescue ''
+                if File.exists?(swatch_image_path)
+                  @product.images <<  Spree::Image.create!(:attachment => File.open(swatch_image_path))
+                  @product.save!
+                end
+              rescue Exception => e
+                puts "#{e.to_s} error loading image rcpbs id #{@rcpbs.id}"
               end
+
+
+              begin
               #create product image
-              image_path = %Q{#{@local_site_path}#{@wi.image_file[/images.*/i,0]}} rescue ''
-              if File.exists?(image_path)
-                found_image = true
-                @product.images <<  Spree::Image.create!(:attachment => File.open(image_path))
-                @product.save!
+                image_path = %Q{#{@local_site_path}#{@wi.image_file[/images.*/i,0]}} rescue ''
+                if File.exists?(image_path)
+                  found_image = true
+                  @product.images <<  Spree::Image.create!(:attachment => File.open(image_path))
+                  @product.save!
+                end
+              rescue Exception => e
+                puts "#{e.to_s} error loading image rcpbs id #{@rcpbs.id}"
               end
+
+
             end
 
             if found_image == false
@@ -521,19 +534,29 @@ BEGIN{
 
               if wi
                 #create product image
-                image_path = %Q{#{@local_site_path}#{wi.image_file[/images.*/i,0]}} rescue ''
-                if File.exists?(image_path)
-                  v.images <<  Spree::Image.create!(:attachment => File.open(image_path))
-                  v.save!
+                begin
+                  image_path = %Q{#{@local_site_path}#{wi.image_file[/images.*/i,0]}} rescue ''
+                  if File.exists?(image_path)
+                    v.images <<  Spree::Image.create!(:attachment => File.open(image_path))
+                    v.save!
+                  end
+                rescue Exception => e
+                  puts "#{e.to_s} error loading image rcpbs id #{rcpbs.id}"
                 end
               else
-                # lets try an image with sku as the name
-                src_sku_image = %Q{#{@local_site_path}/images/#{rcpbs.new_pbs_desc_1.strip}*} rescue ''
-                array_of_found_sku_images = Dir.glob(src_sku_image)
-                if !array_of_found_sku_images.empty?
-                  v.images <<  Spree::Image.create!(:attachment => File.open(array_of_found_sku_images.first))
-                  v.save!
+
+                begin
+                  # lets try an image with sku as the name
+                  src_sku_image = %Q{#{@local_site_path}/images/#{rcpbs.new_pbs_desc_1.strip}*} rescue ''
+                  array_of_found_sku_images = Dir.glob(src_sku_image)
+                  if !array_of_found_sku_images.empty?
+                    v.images <<  Spree::Image.create!(:attachment => File.open(array_of_found_sku_images.first))
+                    v.save!
+                  end
+                rescue Exception => e
+                  puts "#{e.to_s} error loading image rcpbs id #{rcpbs.id}"
                 end
+
               end
 
               logger.info "Variant #{rcpbs.new_pbs_desc_1} created in Spree"
