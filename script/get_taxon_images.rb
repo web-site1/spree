f73344a468e4c7116b9d2f84807b77e045c35e7c
Louie Arencibia <louie728@gmail.com>
2015-03-20 11:58:33 -0400

    - Added script for taxon images - displaying taxon images

---
+++ script/get_taxon_images.rb
@@ -0,0 +1,67 @@
+# pass site directory as an argument /home/louie/Dropbox/DEV/Artistic/sitesucker/www.artisticribbon.com
+# script will attempt to find a file matching the name of the taxon and
+# get the icon picture from the old site
+
+
+require File.expand_path('../../config/environment', __FILE__)
+
+log_file_name = %Q{taxon_images-#{Time.now.strftime("%m%d%y%I%M")}.log}
+log_file =  %Q{#{Rails.root}/log/#{log_file_name}}
+logger = Logger.new(log_file)
+logger.info "Starting get Taxon images"
+
+puts "Starting get Taxon images"
+
+
+site_path = ARGV[0]
+image_path = site_path+'/images'
+
+
+Spree::Taxon.all.each do |t|
+#Spree::Taxon.where(name: 'Gum Drops').each do |t|
+  next if !(t.root.id == 1)
+  srch_name = t.name.downcase.gsub(' ','*')
+
+  files = Dir[%Q{#{site_path}/1-#{srch_name}*category*}]
+  if files.count == 1
+    f_path = files.first.chomp
+    #puts "Processing file: #{f_path}..."
+    uri = URI.parse(f_path)
+    page=Nokogiri::HTML(open(f_path))
+
+    alt_src = t.name.downcase.split(' ').first
+    all_images = page.xpath("//img")
+    im = []
+    all_images.each do |i|
+      next if i[:alt].blank?
+      if i[:alt].downcase.include?(alt_src)
+        im << i
+        break
+      end
+    end
+
+    #im = page.xpath("//td//font//img")
+    #pic_tab = page.xpath("//table")[2]
+    #new_noko = Nokogiri::HTML(pic_tab.to_s)
+    #im = new_noko.xpath("//img")
+
+    if !im.empty?
+      image_src = im.first[:src]
+      if File.file?(%Q{#{site_path}/#{image_src[/images.*/i,0]}})
+        t.icon = File.open(%Q{#{site_path}/#{image_src[/images.*/i,0]}})
+        t.save
+      else
+        puts %Q{Cant find image: page #{f_path} image: #{image_src}}
+        logger.info %Q{Cant find image: page #{f_path} image: #{image_src}}
+      end
+    else
+      puts %Q{Cant get image for page #{f_path}}
+      logger.info %Q{Cant get image for page #{f_path}}
+    end
+
+  else files.count > 1
+    puts %Q{Too many pages found for #{t.name}}
+    logger.info %Q{Too many pages found for #{t.name}}
+
+  end
+end
