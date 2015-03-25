98990fe118c3a4b6c8d244ce9e6d31c5067c1935
Louie Arencibia <louie728@gmail.com>
2015-03-25 17:04:09 -0400

    - script to add missing taxon pictures

--- script/load_missing_taxon_from_webcat.rb
+++ script/load_missing_taxon_from_webcat.rb
@@ -10,17 +10,36 @@ Spree::Taxon.where("icon_file_name is null").each do |t|
   if perm_array.count > 2
     if perm_array.count == 3
       # we are searching a top cat
-      cat = perm_array.last
-      subcat = nil
+      if perm_array[1] == 'tulle-and-trims' || perm_array[1] == 'chevron'
+        cat = perm_array[1]
+        subcat = perm_array[2]
+      else
+        cat = perm_array.last
+        subcat = nil
+      end
     else
       subcat = perm_array.last
       cat = perm_array[perm_array.index(subcat)-1]
+      if subcat == 'metallic' && cat == 'ribbons'
+        cat = 'metallic'
+      end
+
     end
+
+    if cat == 'dotted' && subcat
+      cat = 'dot'
+    end
+
+    cat = cat.downcase.gsub('-and-','-&-')
     cat = cat.gsub('-','%')
     cat += '%'
+    if cat == 'checks%&%plaids%'
+      cat = 'check%&%plaid%'
+    end
     where = "cat like '#{cat}' and subcat "
 
     if subcat
+      subcat = subcat.downcase.gsub('-and-','-&-')
       subcat = subcat.gsub('-','%')
       subcat += '%'
       where += "like '#{subcat}'"
@@ -31,6 +50,8 @@ Spree::Taxon.where("icon_file_name is null").each do |t|
     wc = WebCat.where(where).first
     if wc
       puts %Q{#{t.permalink} image: #{wc.image_file} }
+
+
     else
       puts %Q{#{t.permalink} no match where: #{where} }
     end
