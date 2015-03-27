67aa4415fcfea30667e4058642733ed5709dd51d
Louie Arencibia <louie728@gmail.com>
2015-03-27 10:04:34 -0400

    - Changed option

---
+++ script/tulle_opts.rb
@@ -0,0 +1,7 @@
+require File.expand_path('../../config/environment', __FILE__)
+
+
+# Add options for specific Tulle subcats
+# Tulle, Lacey, Geo Mesh, GeoMesh Cross, glitter maze, love foil
+# options Width and putup
+
