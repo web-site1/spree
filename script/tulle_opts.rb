bb717ee2465124d094ce5d150ec52026295aed94
Louie Arencibia <louie728@gmail.com>
2015-03-27 16:58:25 -0400

    - scripts for tulle

--- script/tulle_opts.rb
+++
@@ -1,7 +0,0 @@
-require File.expand_path('../../config/environment', __FILE__)
-
-
-# Add options for specific Tulle subcats
-# Tulle, Lacey, Geo Mesh, GeoMesh Cross, glitter maze, love foil
-# options Width and putup
-
