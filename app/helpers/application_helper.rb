5e0560a64401a146952b706b7dead78bd870ef42
Dan Sadaka <dan@web-site1.com>
2015-04-01 22:32:01 -0400

    30 mins: Now sort sidebar as so admin user can modify.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -49,7 +49,7 @@ module ApplicationHelper
 
   def side_search_cats
     type_hash = {}
-    @all_tax_cats = Spree::Taxon.where("permalink like '%categories%'")
+    @all_tax_cats = Spree::Taxon.where("permalink like '%categories%'").order(:lft)
     types =  @all_tax_cats.select{|tax| tax.parent_id == 1 }  #Spree::Taxon.where(parent_id: 1)
     types.each do |t|
       eopt = @all_tax_cats.select{|tax| tax.parent_id == t.id } #t.children
