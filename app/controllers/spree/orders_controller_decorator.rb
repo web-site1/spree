67b0bf0f3a1ab4983741cc43327e9fa001407671
Dan Sadaka <dan@web-site1.com>
2015-04-16 20:18:53 -0400

    1 hr: Now "Continue Shopping" returns prodcut page for last item on cart.  Also started on Country dropdown request

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -6,4 +6,11 @@ Spree::OrdersController.class_eval do
     render text: params[:state]
   end
 
+  # Shows the current incomplete order from the session
+  # Now go back to last item on order when user says "Continue Shopping...Dan"
+  def edit
+    @order = current_order || Order.incomplete.find_or_initialize_by(guest_token: cookies.signed[:guest_token])
+    @continue_shopping_path = product_path(@order.line_items.last.variant.product) rescue products_path
+    associate_user
+  end
 end
