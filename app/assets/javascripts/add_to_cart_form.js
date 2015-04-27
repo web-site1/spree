bc0cd973b35920deaa40ffff2475a538d53b9bbe
Louie Arencibia <louie728@gmail.com>
2015-04-27 11:36:55 -0400

    - ajax add to cart

--- app/assets/javascripts/add_to_cart_form.js
+++ app/assets/javascripts/add_to_cart_form.js
@@ -15,8 +15,13 @@ jQuery(document).ready(function ($) {
                     $('#link-to-cart').html(o.responseText);
 
                 }
-                $('#art_message').html(data.mess).fadeIn().delay(3000).fadeOut();
-                $('#add-to-cart-button').prop("disabled", false);
+                $('#art_message').html(data.mess) //.fadeIn().delay(3000).fadeOut();
+                $('#itm_ord').modal('show');
+                setTimeout(function() {   //calls click event after a certain time
+                    $('#itm_ord').modal('hide');
+                    $('#add-to-cart-button').prop("disabled", false);
+                }, 2000);
+
             },
             error: function (data) {
                 alert(data.mess);
