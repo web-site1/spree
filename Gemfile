09e0e92177224b936d324c773a669bfed6d18420
Dan Sadaka <dan@web-site1.com>
2015-06-17 23:50:53 -0400

    3 hrs:  Simplified use of Constant Contact API

--- Gemfile
+++ Gemfile
@@ -48,6 +48,7 @@ group :development, :test do
   gem 'spring'
 
   gem 'sunspot_solr'
+  gem 'bootstrap-generators', '~> 3.3.4'
 end
 
 
--- app/models/ccontact.rb
+++ app/models/ccontact.rb
@@ -15,4 +15,49 @@ class Ccontact
   def add_to_list(list_id)
     @cc.lists << {id: list_id.to_s} unless @cc.lists.map(&:id).include?(list_id.to_s)
   end
+
+  def add_contact(contact, params = {})
+    @c.add_contact(contact, params)
+  end
+
+  def get_contact_by_email(email)
+    contacts = get_contacts(email: email)
+    contacts.results[0]
+  end
+
+  def get_contacts(param)
+    @c.get_contacts(param)
+  end
+
+  # Update attributes of contact and return updated object
+  def update_contact(contact)
+    @c.update_contact(contact)
+    find(contact.id)
+  end
+
+  # Delete a contact from a list and return updated object
+  # Passed:
+  #   contact:  contact object (contact must exist)
+  #   list_id:  id of list (integer or string)
+  def delete_contact_from_list(contact, list_id)
+    @c.delete_contact_from_list(contact.id.to_i, list_id.to_i) if in_list?(contact, list_id)
+    find(contact.id)
+  end
+
+  def in_list?(contact, list_id)
+    contact.lists.map(&:id).include?(list_id.to_s)
+  end
+
+  def get_lists(params = {})
+    @c.get_lists(params)
+  end
+
+  def add_contact_to_list(contact, list_id)
+    unless in_list?(contact, list_id)
+      contact.lists << {id: list_id.to_s}
+      contact = @c.update_contact(contact)
+    end
+    contact
+  end
+
 end
