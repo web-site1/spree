a9e5a494485d030acdc7e5d5f063c4fb8c1ce20f
Dan Sadaka <dan@web-site1.com>
2016-08-08 02:19:53 -0400

    30 mins: Added text for 50% off sale and deployed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,10 +10,10 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports and disable caching.
-  # config.consider_all_requests_local       = true
-  config.consider_all_requests_local       = false
-  # config.action_controller.perform_caching = false
-  config.action_controller.perform_caching = true
+  config.consider_all_requests_local       = true
+  # config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = false
+  # config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -27,7 +27,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = false #true
+  config.assets.debug = true
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
